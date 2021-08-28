90ac5a87d31f0d2a8983db37b780fd63d4ec46ff
ibrahimFN <89642812+ibrahimFN@users.noreply.github.com>
2021-08-28 16:51:04 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+
+
+Client.login("")
+
+Client.on("ready"), () => {
+console.log('bot is ${Client.user.tag} online');
+});
