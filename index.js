fffff928fa59135e2bf6d8cca9020c7a97d60a4e
ibrahimFN <89642812+ibrahimFN@users.noreply.github.com>
2021-08-28 17:25:50 +0300

    Create index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 const Client = new Discord.Client();
-
+const prefix = "#";
 
 Client.login("")
 
 Client.on("ready"), () => {
 console.log('bot is ${Client.user.tag} online');
+   Client.user.setActivity('#help', { type:"STREAMING" });
 });
